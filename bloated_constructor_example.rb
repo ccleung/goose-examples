f43a12ce43fafc8e01204d40f8f0335cff2bc196
Clement Leung <clement.leung@shopify.com>
2018-04-24 21:23:29 -0400

    group LocationFinder, DomesticNotifier, ImportedNotifier together

--- bloated_constructor_example.rb
+++ bloated_constructor_example.rb
@@ -4,31 +4,42 @@ class MessageProcessor
   def initialize(
     unpacker: UnPacker.new(CounterPartyFinder.new),
     auditor: Auditor.new,
-    location_finder: LocationFinder.new,
-    domestic_notifier: DomesticNotifier.new,
-    imported_notifier: ImportedNotifier.new
+    dispatcher: MessageDispatcher.new
   )
     @unpacker = unpacker
     @auditor = auditor
-    @location_finder = location_finder
-    @domestic_notifier = domestic_notifier
-    @imported_notifier = imported_notifier
+    @dispatcher = dispatcher
   end
 
   def on_message(message)
     unpacked = unpacker.unpack(message)
     auditor.record_receipt_of(unpacked)
 
-    if location_finder.is_domestic(unpacked)
-      domestic_notifier.notify(unpacked.as_domestic_message)
-    else
-      imported_notifier.notify(unpacked.as_imported_message)
-    end
+    dispatcher.dispatch(unpacked)
   end
 
   private
 
   attr_reader :unpacker, :auditor
-  attr_reader :location_finder, :domestic_notifier, :imported_notifier
+  attr_reader :dispatcher
 end
 
+class MessageDispatcher
+  def initialize(
+    location_finder: LocationFinder.new,
+    domestic_notifier: DomesticNotifier.new,
+    imported_notifier: ImportedNotifier.new
+  )
+    @location_finder = location_finder
+    @domestic_notifier = domestic_notifier
+    @imported_notifier = imported_notifier
+  end
+
+  def dispatch(unpacked_message)
+    if location_finder.is_domestic(unpacked_message)
+      domestic_notifier.notify(unpacked_message.as_domestic_message)
+    else
+      imported_notifier.notify(unpacked_message.as_imported_message)
+    end
+  end
+end
